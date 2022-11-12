9cf2a7d901fa50c97a73f2ae5c69c06b260a8a36
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-11-12 21:24:17 +0530

    Added: final match statck is Won and loss

--- src/com/bridgelabz/com/Gambler.java
+++ src/com/bridgelabz/com/Gambler.java
@@ -1,13 +1,24 @@
 package com.bridgelabz.com;
 
 public class Gambler {
+	public static void main(String[] args) {
+     int STAKE = 100;
+     int BET = 1;
+    
+        System.out.println("Welcome to Gambling Game");
+        int print = 0;
+        int start = (int) (Math.random() * 2);
 
-	
-		int Stake = 100;
-		int Bets = 1;
-		public static void main(String args)
-		{
-		System.out.println("Welcome to gambling game");
-		
-	}
+        if (start == 1){
+            System.out.println("You have won the stake == " + STAKE);
+            
+            print = STAKE + BET;
+            System.out.println("You now have == " + print);
+        }else {
+            System.out.println("You have loss the stake == " + STAKE);
+           
+            print = STAKE - BET;
+            System.out.println(" You now have == " + print);
+        }
+    }
 }
