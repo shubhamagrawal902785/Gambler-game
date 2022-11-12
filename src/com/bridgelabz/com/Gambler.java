801e2824989fc152e66fdd66a965aa3199c0aaae
shubhamagrawal902785 <114243771+shubhamagrawal902785@users.noreply.github.com>
2022-11-12 20:54:33 +0530

    Update Gambler.java

--- src/com/bridgelabz/com/Gambler.java
+++ src/com/bridgelabz/com/Gambler.java
@@ -1,12 +1,12 @@
 package com.bridgelabz.com;
 
 public class Gambler {
-
+public static void main(String args)
+		{
 	
 		int Stake = 100;
 		int Bets = 1;
-		public static void main(String args)
-		{
+		
 		System.out.println("Welcome to gambling game");
 		
 	}
