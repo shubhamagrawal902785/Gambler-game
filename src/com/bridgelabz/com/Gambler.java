8d21bb3a895355b007c59d928c8e00f104ccbfaf
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-11-12 20:53:02 +0530

    Welcome to gambling game

---
+++ src/com/bridgelabz/com/Gambler.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.com;
+
+public class Gambler {
+
+	
+		int Stake = 100;
+		int Bets = 1;
+		public static void main(String args)
+		{
+		System.out.println("Welcome to gambling game");
+		
+	}
+}
